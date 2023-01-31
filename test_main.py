7c609f03bd73bd2339f5051d445a2d8d9129fefe
ArmandoLPollo <61497361+ArmandoLPollo@users.noreply.github.com>
2023-01-31 17:16:07 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,5 +7,17 @@ class FlaskTestCase(unittest.TestCase):
     def create_app(self):
             return app
         
+    def test_classify(self):
+        input_data = {"pixels": [0 for i in range(784)]}
+        
+        print('1')
+        response = requests.post("http://localhost:5000/classify", json=input_data)
+        print('2')
+        result = response.json()
+        print('3')
+        expected_result = {"class": 0}
+        print('4')
+        assert result == expected_result, f"Expected {expected_result} but got {result}"
+        
 if __name__ == '__main__':
     unittest.main()
