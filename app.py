0ab5be7c8588672cc605b5ce781c6edb8df1c6b5
ArmandoLPollo <armanddrouineau92@gmail.com>
2023-01-31 15:50:32 +0100

    add files

---
+++ app.py
@@ -0,0 +1,61 @@
+
+"""
+simple python flask application
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+import os
+from flask import Flask, request
+from flask import render_template
+from flask import url_for
+from flask.json import jsonify
+import pickle
+import numpy as np
+
+##########################################################################
+## Application Setup
+##########################################################################
+
+app = Flask(__name__)
+
+##########################################################################
+## Loa model & define classes
+##########################################################################
+
+model = pickle.load(open("trained_model.pkl", "rb"))
+classes = ['T-shirt', 'Trouser', 'Pullover', 'Dress','Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+##########################################################################
+## Routes
+##########################################################################
+
+@app.route("/classify", methods=["POST"])
+def classify():
+    
+    # Get the data from the request
+    data = request.get_json()
+    
+    # Convert the data to a numpy array
+    pixels = np.array(data["pixels"])
+    
+    # Reshape the array to match the model's input shape
+    pixels = pixels.reshape(1, -1)
+    
+    # Use the model to make a prediction
+    prediction = model.predict(pixels)[0]
+    
+    # Get the index of the class with the highest prediction
+    class_index = np.argmax(prediction)
+    
+    # Return the predicted class as a response
+    return {"class": classes[class_index]}
+
+##########################################################################
+## Main
+##########################################################################
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test_main.py
@@ -0,0 +1,31 @@
+import unittest
+from app import app
+
+class FlaskTestCase(unittest.TestCase):
+
+    def create_app(self):
+            return app
+
+        def test_classify(self):
+            # Define the API endpoint URL
+            url = "http://localhost:5000/classify"
+
+            # Define the data to send in the request using list comprehension
+            data = {
+                "pixels": [0.0 for i in range(784)]
+            }
+
+            # Send a POST request to the API endpoint
+            response = requests.post(url, json=data)
+
+            # Get the response data
+            response_data = json.loads(response.text)
+
+            # Define the expected result
+            expected_result = {"class": "T-shirt"}
+
+            # Compare the result of the API with the expected value
+            self.assertEqual(response_data, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
