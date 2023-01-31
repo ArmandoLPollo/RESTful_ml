a3473d9e1ae72237dff16828a83843548f691b37
ArmandoLPollo <61497361+ArmandoLPollo@users.noreply.github.com>
2023-01-31 16:19:03 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,26 +6,26 @@ class FlaskTestCase(unittest.TestCase):
     def create_app(self):
             return app
 
-        def test_classify(self):
-            # Define the API endpoint URL
-            url = "http://localhost:5000/classify"
+    def test_classify(self):
+        # Define the API endpoint URL
+        url = "http://localhost:5000/classify"
 
-            # Define the data to send in the request using list comprehension
-            data = {
-                "pixels": [0.0 for i in range(784)]
-            }
+        # Define the data to send in the request using list comprehension
+        data = {
+            "pixels": [0.0 for i in range(784)]
+        }
 
-            # Send a POST request to the API endpoint
-            response = requests.post(url, json=data)
+        # Send a POST request to the API endpoint
+        response = requests.post(url, json=data)
 
-            # Get the response data
-            response_data = json.loads(response.text)
+        # Get the response data
+        response_data = json.loads(response.text)
 
-            # Define the expected result
-            expected_result = {"class": "T-shirt"}
+        # Define the expected result
+        expected_result = {"class": "T-shirt"}
 
-            # Compare the result of the API with the expected value
-            self.assertEqual(response_data, expected_result)
+        # Compare the result of the API with the expected value
+        self.assertEqual(response_data, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
